f20000c73133323b312f0deceabcf478ad9c6951
Ezequiel Tamayo <EzequielTamayo@gmail.com>
2025-11-06 20:37:22 +0100

    DB conexion with environment variables

---
+++ aprobapp/src/main/java/com/aprobapp/aprobapp/AprobappApplication.java
@@ -0,0 +1,13 @@
+package com.aprobapp.aprobapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprobappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprobappApplication.class, args);
+	}
+
+}
---
+++ aprobapp/src/test/java/com/aprobapp/aprobapp/AprobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprobapp.aprobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
